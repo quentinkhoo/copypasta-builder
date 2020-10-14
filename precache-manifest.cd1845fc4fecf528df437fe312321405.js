6a38dbaea34f76a39ae1c0dbcab20da466a97d03
Quentin Khoo <quentinkhoo@outlook.com>
2020-10-15 04:05:18 +0800

    Updates

---
+++ precache-manifest.cd1845fc4fecf528df437fe312321405.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e234f53c29bf5c3247e6e83d74b4e7",
+    "url": "/copypasta-builder/index.html"
+  },
+  {
+    "revision": "2b42a03ef7a493d62cec",
+    "url": "/copypasta-builder/static/css/main.21b8d74c.chunk.css"
+  },
+  {
+    "revision": "4e4ce293a7cf781e6f5d",
+    "url": "/copypasta-builder/static/js/2.5133f598.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/copypasta-builder/static/js/2.5133f598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b42a03ef7a493d62cec",
+    "url": "/copypasta-builder/static/js/main.cdf03096.chunk.js"
+  },
+  {
+    "revision": "d5b4d5aeacd972a544f5",
+    "url": "/copypasta-builder/static/js/runtime-main.edd72462.js"
+  },
+  {
+    "revision": "817a8b9bf755d200a886a7285d335a62",
+    "url": "/copypasta-builder/static/media/pepewallpaper.817a8b9b.png"
+  }
+]);

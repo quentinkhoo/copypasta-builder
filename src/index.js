521f2b921b36f0665fe3cfa848c88002c504425e
terftw <68211558+terftw@users.noreply.github.com>
2021-05-07 16:45:51 +0800

    changed homepage

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import 'semantic-ui-css/semantic.min.css'
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
